d1e5c15cbb34a546468204a31dea0f6a824da16d
Gustav Pursche <pursche@posteo.de>
2018-08-04 19:40:43 +0200

    Language-switcher: highlight current language

--- language-switcher.php
+++ language-switcher.php
@@ -1,11 +1,11 @@
 <ul class="language-switcher">
   <?php
-    $languages = pll_the_languages( array( 'raw' => 1 ) );
-    foreach($languages as $language):
+    foreach(pll_the_languages( array( 'raw' => 1 ) ) as $language):
+      $is_current_lang = $language['current_lang'];
   ?>
       <li class="language-switcher__item">
         <a href="<?php echo $language['url']; ?>"
-          class="language-switcher__language">
+          class="language-switcher__language <?php if ($is_current_lang) { echo 'language-switcher__language--is-active'; } ?>">
           <?php echo $language['name']; ?>
         </a>
       </li>
