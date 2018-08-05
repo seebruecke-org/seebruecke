7872aa5933968b29a35bb64e5109448b3a2fc9ad
Gustav Pursche <pursche@posteo.de>
2018-08-05 20:56:12 +0200

    BecomeSupporter: add shortcode

--- functions.php
+++ functions.php
@@ -216,6 +216,40 @@ function shortcode_actions() {
     </ul>';
 }
 
+function shortcode_become_supporter($atts = [], $content = null) {
+  $atts = array_change_key_case((array)$atts, CASE_LOWER);
+
+  return '
+    <ol class="become-supporter">'
+      . do_shortcode($content) .
+    '</ol>
+  ';
+}
+
+function shortcode_become_supporter_item($atts = [], $content = null) {
+  $atts = array_change_key_case((array)$atts, CASE_LOWER);
+
+  $icons = array(
+    'flag' => '<svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M349.565 98.783C295.978 98.783 251.721 64 184.348 64c-24.955 0-47.309 4.384-68.045 12.013a55.947 55.947 0 0 0 3.586-23.562C118.117 24.015 94.806 1.206 66.338.048 34.345-1.254 8 24.296 8 56c0 19.026 9.497 35.825 24 45.945V488c0 13.255 10.745 24 24 24h16c13.255 0 24-10.745 24-24v-94.4c28.311-12.064 63.582-22.122 114.435-22.122 53.588 0 97.844 34.783 165.217 34.783 48.169 0 86.667-16.294 122.505-40.858C506.84 359.452 512 349.571 512 339.045v-243.1c0-23.393-24.269-38.87-45.485-29.016-34.338 15.948-76.454 31.854-116.95 31.854z"></path></svg>',
+    'bulhorn' => '<svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M576 224c0-20.896-13.36-38.666-32-45.258V64c0-35.346-28.654-64-64-64-64.985 56-142.031 128-272 128H48c-26.51 0-48 21.49-48 48v96c0 26.51 21.49 48 48 48h43.263c-18.742 64.65 2.479 116.379 18.814 167.44 1.702 5.32 5.203 9.893 9.922 12.88 20.78 13.155 68.355 15.657 93.773 5.151 16.046-6.633 19.96-27.423 7.522-39.537-18.508-18.026-30.136-36.91-19.795-60.858a12.278 12.278 0 0 0-1.045-11.673c-16.309-24.679-3.581-62.107 28.517-72.752C346.403 327.887 418.591 395.081 480 448c35.346 0 64-28.654 64-64V269.258c18.64-6.592 32-24.362 32-45.258zm-96 139.855c-54.609-44.979-125.033-92.94-224-104.982v-69.747c98.967-12.042 169.391-60.002 224-104.982v279.711z"></path></svg>',
+    'users' => '<svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm32 32h-64c-17.6 0-33.5 7.1-45.1 18.6 40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64zm-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zm-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4z"></path></svg>',
+  );
+  $icon = $atts['icon'];
+  $icon_markup = $icons[$icon];
+  $title = $atts['title'];
+  $title_markup = $title ? '<h3 class="become-supporter__title">' . $title . '</h3>' : '';
+
+  return '
+    <li class="become-supporter__item">
+      <div class="become-supporter__icon-container">
+        ' . $icon_markup . '
+      </div>
+      ' . $title_markup . '
+      <div class="become-supporter__content">'. $content . '</div>
+    </li>
+  ';
+}
+
 function cleanup_admin() {
   remove_menu_page('edit.php');
   remove_menu_page('edit-comments.php');
@@ -239,6 +273,9 @@ add_action('admin_menu','cleanup_admin');
 
 add_shortcode('actions', 'shortcode_actions');
 add_shortcode('donate', 'shortcode_donate');
+add_shortcode('become_supporter', 'shortcode_become_supporter');
+add_shortcode('become_supporter_item', 'shortcode_become_supporter_item');
+
 add_action('wp_enqueue_scripts', 'enqueue_style');
 
 /* image sizes */
