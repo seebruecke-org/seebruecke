17fbeb51cde35348091963bf3e97a152d4aa647e
Gustav Pursche <pursche@posteo.de>
2018-08-06 15:51:58 +0200

    LanguageSwitcher: enhance styling

--- language-switcher.php
+++ language-switcher.php
@@ -1,15 +1,21 @@
-<ul class="language-switcher">
-  <?php
-    foreach(pll_the_languages( array( 'raw' => 1 ) ) as $language):
-      $is_current_lang = $language['current_lang'];
-  ?>
-      <li class="language-switcher__item">
-        <a href="<?php echo $language['url']; ?>"
-          class="language-switcher__language <?php if ($is_current_lang) { echo 'language-switcher__language--is-active'; } ?>">
-          <?php echo $language['name']; ?>
-        </a>
-      </li>
-  <?php
-    endforeach;
-  ?>
-</ul>
+<div class="language-switcher">
+  <p class="language-switcher__label">
+    Sprache:
+  </p>
+
+  <ul class="language-switcher__languages">
+    <?php
+      foreach(pll_the_languages( array( 'raw' => 1 ) ) as $language):
+        $is_current_lang = $language['current_lang'];
+    ?>
+        <li class="language-switcher__item">
+          <a href="<?php echo $language['url']; ?>"
+            class="language-switcher__language <?php if ($is_current_lang) { echo 'language-switcher__language--is-active'; } ?>">
+            <?php echo $language['name']; ?>
+          </a>
+        </li>
+    <?php
+      endforeach;
+    ?>
+  </ul>
+</div>
