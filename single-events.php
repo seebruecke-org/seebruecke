bdeb5b20c90f9846eb265a0b0f68c6c712d5ca90
Gustav Pursche <pursche@posteo.de>
2018-08-08 23:58:34 +0200

    Back: add back to archive link on single event pages

--- single-events.php
+++ single-events.php
@@ -55,6 +55,19 @@
           </div>
 
           <?php the_content(); ?>
+
+          <?php
+            $slug = pll_current_language('slug');
+            $slug = $slug == 'de' ? '' : $slug;
+            $url = $slug . '/events/';
+          ?>
+
+          <div class="back">
+            <a href="<?php echo $url; ?>" class="back__link">
+              <svg aria-hidden="true" class="back__icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path></svg>
+              <?php echo pll__('Alle Events'); ?>
+            </a>
+          </div>
         </article>
       </div>
     </div>
