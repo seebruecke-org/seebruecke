ae674731aea782c6d12bc800e908e1cc4f1e5638
Gustav Pursche <pursche@posteo.de>
2018-11-03 12:19:24 +0100

    Add archive page for localgroups & begin work on single page

---
+++ lib/events.php
@@ -0,0 +1,220 @@
+<?php
+
+function get_all_events($extend_query = []) {
+  $args = array_merge(
+    array(
+      'meta_query' => array(
+        array(
+          'key' => 'event_date',
+        )
+      ),
+      'orderby' => 'event_date',
+      'order' => 'DESC',
+      'post_type' => 'events',
+      'post_status' => 'publish',
+      'posts_per_page' => -1,
+    ),
+    $extend_query
+  );
+
+  $query = new WP_Query($args);
+
+  return $query;
+}
+
+function get_all_upcoming_events() {
+  return get_all_events(array(
+    'order' => 'ASC',
+    'meta_query' => array(
+      array(
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => date('Y-m-d'),
+        'type' => 'DATE',
+      )
+    ),
+  ));
+}
+
+function get_all_upcoming_events_by_tags($tags) {
+  return get_all_events(array(
+    'order' => 'ASC',
+    'tag' => $tags,
+    'meta_query' => array(
+      array(
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => date('Y-m-d'),
+        'type' => 'DATE',
+      )
+    ),
+  ));
+}
+
+function shortcode_actions($atts = []) {
+  function group_events_by_date($events) {
+    $grouped = array();
+
+    foreach($events as $event) {
+      $id = $event->ID;
+      $fields = get_post_custom($id);
+
+      $grouped[$fields['event_date'][0]][] = $event;
+    }
+
+    return $grouped;
+  }
+
+  function render_events($events) {
+    $markup = '';
+
+    foreach($events as $event) {
+      $id = $event->ID;
+      $fields = get_post_custom($id);
+      $href = get_the_permalink($id);
+
+      $markup .= '
+        <li class="actions__action js-action">
+          <div class="action">
+            <div class="action__icon-container">
+              <a href="' . $href . '" rel="nofollow">
+              <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M576 224c0-20.896-13.36-38.666-32-45.258V64c0-35.346-28.654-64-64-64-64.985 56-142.031 128-272 128H48c-26.51 0-48 21.49-48 48v96c0 26.51 21.49 48 48 48h43.263c-18.742 64.65 2.479 116.379 18.814 167.44 1.702 5.32 5.203 9.893 9.922 12.88 20.78 13.155 68.355 15.657 93.773 5.151 16.046-6.633 19.96-27.423 7.522-39.537-18.508-18.026-30.136-36.91-19.795-60.858a12.278 12.278 0 0 0-1.045-11.673c-16.309-24.679-3.581-62.107 28.517-72.752C346.403 327.887 418.591 395.081 480 448c35.346 0 64-28.654 64-64V269.258c18.64-6.592 32-24.362 32-45.258zm-96 139.855c-54.609-44.979-125.033-92.94-224-104.982v-69.747c98.967-12.042 169.391-60.002 224-104.982v279.711z"></path></svg>
+              </a>
+            </div>
+
+            <div class="action__content-container">
+              <h3 class="action__title">
+                <div class="action__meta">
+                  <small class="action__time">'
+                    . $fields['event_time'][0] .
+                  '</small>
+                </div>
+
+                <a href="' . $href . '">
+                  ' . $fields['event_city'][0] . '
+                </a>
+              </h3>
+
+              <p class="action__content">' . $event->post_title . '</p>
+            </div>
+          </div>
+        </li>
+      ';
+    }
+
+    return '
+      <ul class="actions__events">' . $markup . '</ul>
+    ';
+  }
+
+  function render_days($show_only_upcoming, $show_upcoming_count, $filter_by_tags) {
+    $markup = '';
+
+    if ($show_only_upcoming) {
+      if ($filter_by_tags) {
+        $events = get_all_upcoming_events_by_tags($filter_by_tags);
+      } else {
+        $events = get_all_upcoming_events();
+      }
+    } else {
+      $events = get_all_events();
+    }
+
+    $events_grouped = group_events_by_date($events->posts);
+
+    if($show_upcoming_count) {
+      $events_grouped = array_slice($events_grouped, 0, (int)$show_upcoming_count);
+    }
+
+    foreach($events_grouped as $date => $event) {
+      $id = $event->ID;
+      $fields = get_post_custom($id);
+
+      $markup .= '
+        <li class="actions__day">
+          <h3 class="actions__day-title">
+            ' . date(get_date_format(), strtotime($date)) . '
+          </h3>
+          ' . render_events($event) . '
+        </li>
+      ';
+    }
+
+    return $markup;
+  }
+
+  $atts = array_change_key_case((array)$atts, CASE_LOWER);
+  $slug = pll_current_language('slug');
+  $slug = $slug == 'de' ? '' : ( '/' . $slug );
+  $url = $slug . '/events/';
+  $all_markup = '';
+
+  if (!is_archive()) {
+    $all_markup = '
+      <a href="' . $url . '" class="actions__more">
+        ' . pll__('Alle Aktionen') . '
+      </a>
+    ';
+  }
+
+  $show_only_upcoming = array_key_exists('upcoming', $atts) || in_array('upcoming', $atts);
+  $show_upcoming_count = $atts['upcoming'];
+  $filter_by_tags = $atts['tags'];
+
+  // map coordinates
+  if ($filter_by_tags) {
+    $events = get_all_upcoming_events_by_tags($filter_by_tags);
+  } else {
+    $events = get_all_upcoming_events();
+  }
+
+  $events_json = [];
+
+  foreach($events->posts as $event) {
+    $id = $event->ID;
+    $fields = get_post_custom($id);
+    $coordinates = $fields['event_coordinates'][0];
+
+    if($coordinates) {
+      $events_json[] = array(
+        'coordinates' => $coordinates,
+        'title' => get_the_title($id),
+        'url' => get_the_permalink($id),
+      );
+    }
+  }
+
+  return '
+    <div class="actions">
+      <div class="map">
+        <div class="map__canvas js-map"
+            data-data=\'' . json_encode($events_json) . '\'></div>
+      </div>
+      <ul class="actions__list">
+      ' . render_days($show_only_upcoming, $show_upcoming_count, $filter_by_tags) . '
+      </ul>
+      ' . $all_markup . '
+    </div>
+  ';
+}
+
+function feed_events() {
+  $post_types = array(
+    'events'
+  );
+
+  foreach($post_types as $post_type) {
+    $feed = get_post_type_archive_feed_link($post_type);
+
+    if ($feed === '' || !is_string( $feed )) {
+      $feed = get_bloginfo('rss2_url') . '?post_type=' . $post_type;
+    }
+
+    printf(
+      __('<link rel="%1$s" type="%2$s" href="%3$s" />'), 'alternate', 'application/rss+xml', $feed);
+  }
+}
+
+add_shortcode('actions', 'shortcode_actions');
+
+?>
--- single-groups.php
+++ single-groups.php
@@ -1,10 +1,80 @@
 <?php get_header(); ?>
 
 <main class="main">
-  <?php while ( have_posts() ) : the_post(); ?>
+  <?php while ( have_posts() ) :
+    the_post();
+    $fields = get_post_custom();
+
+  ?>
     <div class="constraint">
       <div class="action-single">
         <article class="richtext">
+          <div class="action-single__meta-container">
+            <h2><?php echo pll__('Nächste Aktion'); ?></h2>
+
+            <div class="constraint">
+              <dl class="action-single__meta">
+                <?php if($fields['event_date']) : ?>
+                  <dt class="action-single__meta-term">
+                    <span class="visually-hidden">
+                      <?php echo pll__('Uhrzeit'); ?>
+                    </span>
+                  </dt>
+                  <dd>
+                    <p class="action-single__meta-value">
+                      <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"></path></svg>
+
+                      <?php
+                        echo date(get_date_format(), strtotime($fields['event_date'][0]));
+                        echo '&nbsp;' . pll__('um') . '&nbsp;';
+                        echo $fields['event_time'][0];
+                      ?>
+                    </p>
+                  </dd>
+                <?php endif; ?>
+
+                <dt class="action-single__meta-term">
+                  <span class="visually-hidden">
+                    <?php echo pll__('Ort'); ?>
+                  </span>
+                </dt>
+
+                <dd>
+                  <p class="action-single__meta-value">
+                    <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 288 512"><path fill="currentColor" d="M112 316.94v156.69l22.02 33.02c4.75 7.12 15.22 7.12 19.97 0L176 473.63V316.94c-10.39 1.92-21.06 3.06-32 3.06s-21.61-1.14-32-3.06zM144 0C64.47 0 0 64.47 0 144s64.47 144 144 144 144-64.47 144-144S223.53 0 144 0zm0 76c-37.5 0-68 30.5-68 68 0 6.62-5.38 12-12 12s-12-5.38-12-12c0-50.73 41.28-92 92-92 6.62 0 12 5.38 12 12s-5.38 12-12 12z"></path></svg>
+
+                    <?php
+                      $address = $fields['event_city'][0];
+
+                      if ($fields['event_address'][0]) {
+                        $address .= ', ' . $fields['event_address'][0];
+                      }
+
+                      echo $address;
+                    ?>
+                  </p>
+                </dd>
+
+                <?php if($fields['event_link']) : ?>
+                  <dt class="action-single__meta-term">
+                    <span class="visually-hidden">
+                      <?php echo pll__('Link'); ?>
+                    </span>
+                  </dt>
+                  <dd>
+                    <p class="action-single__meta-value">
+                      <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M326.612 185.391c59.747 59.809 58.927 155.698.36 214.59-.11.12-.24.25-.36.37l-67.2 67.2c-59.27 59.27-155.699 59.262-214.96 0-59.27-59.26-59.27-155.7 0-214.96l37.106-37.106c9.84-9.84 26.786-3.3 27.294 10.606.648 17.722 3.826 35.527 9.69 52.721 1.986 5.822.567 12.262-3.783 16.612l-13.087 13.087c-28.026 28.026-28.905 73.66-1.155 101.96 28.024 28.579 74.086 28.749 102.325.51l67.2-67.19c28.191-28.191 28.073-73.757 0-101.83-3.701-3.694-7.429-6.564-10.341-8.569a16.037 16.037 0 0 1-6.947-12.606c-.396-10.567 3.348-21.456 11.698-29.806l21.054-21.055c5.521-5.521 14.182-6.199 20.584-1.731a152.482 152.482 0 0 1 20.522 17.197zM467.547 44.449c-59.261-59.262-155.69-59.27-214.96 0l-67.2 67.2c-.12.12-.25.25-.36.37-58.566 58.892-59.387 154.781.36 214.59a152.454 152.454 0 0 0 20.521 17.196c6.402 4.468 15.064 3.789 20.584-1.731l21.054-21.055c8.35-8.35 12.094-19.239 11.698-29.806a16.037 16.037 0 0 0-6.947-12.606c-2.912-2.005-6.64-4.875-10.341-8.569-28.073-28.073-28.191-73.639 0-101.83l67.2-67.19c28.239-28.239 74.3-28.069 102.325.51 27.75 28.3 26.872 73.934-1.155 101.96l-13.087 13.087c-4.35 4.35-5.769 10.79-3.783 16.612 5.864 17.194 9.042 34.999 9.69 52.721.509 13.906 17.454 20.446 27.294 10.606l37.106-37.106c59.271-59.259 59.271-155.699.001-214.959z"></path></svg>
+
+                      <a href="<?php echo $fields['event_link'][0]; ?>" rel="nofollow" itemprop="url">
+                        <?php echo pll__('Link zum Event'); ?>
+                      </a>
+                    </p>
+                  </dd>
+                <?php endif; ?>
+              </dl>
+            </div>
+          </div>
+
           <?php the_content(); ?>
         </article>
       </div>
