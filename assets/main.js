33464d86c81890732ded67146acc994af688dcf4
Gustav Pursche <pursche@posteo.de>
2019-10-16 13:46:42 +0200

    Menu: remove legacy js for the burger toggle

--- assets/main.js
+++ assets/main.js
@@ -4,11 +4,10 @@ import './styles/index.scss';
 import './images/favicon.png';
 
 import map from './scripts/map';
-import menu from './scripts/menu';
 
 // we have to expose the map for the API callback
 window.googleMaps = map;
 
 domReady().then(() => {
-  menu();
+  map();
 });
